4b51685ae600eb02a4f61920d58a90f789c4070e
Diegocpx <diegoc.paixao@gmail.com>
2022-11-12 09:28:57 -0300

    ERs

---
+++ ChapterBet9/ChapterBet9/Controllers/LivrosController.cs
@@ -0,0 +1,67 @@
+﻿using ChapterBet9.Models;
+using ChapterBet9.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterBet9.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly LivroRepository livroRepository;
+        private LivroRepository _LivroRepository;
+
+        public LivrosController(LivroRepository livroRepository)
+        {
+            _LivroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_LivroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livro = _LivroRepository.BuscarPorId(id);
+
+                if (Livro == Null)
+                {
+                    Return NotFound();
+                }
+                Return Ok(Livro);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _LivroRepository.Cadastrar(livro);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ ChapterBet9/ChapterBet9/Repositories/LivroRepository.cs
@@ -0,0 +1,49 @@
+﻿using ChapterBet9.Contexts;
+using ChapterBet9.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ChapterBet9.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastrar(Livro Livro)
+        {
+            _context.Livros.Add(Livro);
+
+            _context.SaveChanges();
+        }
+
+        public void Atualizar(int id, Livro livro)
+        {
+            Livro LivroBuscado = _context.Livros.Find(id);
+
+            if (LivroBuscado != null)
+            {
+                LivroBuscado.Titulo = livro.Titulo;
+                LivroBuscado.QuantidadedePaginas = livro.QuantidadedePaginas;
+                LivroBuscado.Disponivel = livro.Disponivel;
+            }
+            Return Ok(Livro);
+        }
+    }
+}
