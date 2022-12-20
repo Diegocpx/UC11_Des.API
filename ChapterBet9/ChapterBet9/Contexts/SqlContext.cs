31d4c98e95544ea1921de07443ed2138db224ef6
Diego Paixao <diegoc.paixao@gmail.com>
2022-12-20 12:04:13 -0300

    ER3

--- ChapterBet9/ChapterBet9/Contexts/SqlContext.cs
+++ ChapterBet9/ChapterBet9/Contexts/SqlContext.cs
@@ -9,7 +9,7 @@ namespace ChapterBet9.Contexts
         public SqlContext()
         {
         }
-        public SqlContext(DbContextOptions<SqlContext>options) : base(options)
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
         {
         }
 
@@ -20,11 +20,13 @@ namespace ChapterBet9.Contexts
             if (!optionsBuilder.IsConfigured)
             {
                 // cada provedor tem sua sintaxe para especificação
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-IU2PU2E\\SQLEXPRESS; initial catalog=Chapter; Integrated Security=true");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-EBT13RK\\SQLEXPRESS; initial catalog=Chapter; Integrated Security=true");
                 //dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
             }
         }
-public DbSet<Livro> Livros { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
     }
 }
- 
